c828e30cfc8b97a8d72dc4b0f179a868ef05a191
abdulrhman-elghnam <abdulrhman.mohamed.atya@gmail.com>
2026-09-15 15:34:26 +0300

    refactor: rename parameters for clarity in compare and hash functions

--- server/source/common/main/security/main/compare.security.js
+++ server/source/common/main/security/main/compare.security.js
@@ -1,5 +1,5 @@
 import bcrypt from 'bcrypt';
 
-export const compare = async (text, hashedText) => {
-  return await bcrypt.compare(text, hashedText);
+export const compare = async (plainText, hashedText) => {
+  return await bcrypt.compare(plainText, hashedText);
 };
--- server/source/common/main/security/main/hash.security.js
+++ server/source/common/main/security/main/hash.security.js
@@ -1,6 +1,8 @@
 import { config } from '#/configuration/main/configuration.js';
 import bcrypt from 'bcrypt';
 
-export const hash = async (text) => {
-  return await bcrypt.hash(text, config.HASH_SALT);
+export const hash = async (plainText) => {
+  const salt = bcrypt.genSaltSync(config.HASH_SALT);
+  const hash = bcrypt.hashSync(plainText, salt);
+  return hash;
 };
