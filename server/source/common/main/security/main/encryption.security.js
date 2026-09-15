3be2aa402daef127734d04ccd7eb2c14be3fac57
abdulrhman-elghnam <abdulrhman.mohamed.atya@gmail.com>
2026-09-15 15:35:28 +0300

    refactor: remove unnecessary whitespace in encryption and decryption functions

--- server/source/common/main/security/main/encryption.security.js
+++ server/source/common/main/security/main/encryption.security.js
@@ -4,9 +4,7 @@ import crypto from 'crypto';
 
 export const encrypt = (text) => {
   const key = Buffer.from(config.ENCRYPTION_SECRET_KEY, 'hex');
-
   const iv = crypto.randomBytes(config.IV_LENGTH);
-
   const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
 
   let encryptedData = cipher.update(text, 'utf8', 'hex');
